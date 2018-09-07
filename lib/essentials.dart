e0d6c48dbff2093c4e7ae9f4b8257e8c370389fd
Martijn van Dijk <mhvdijk@gmail.com>
2018-09-07 12:09:50 +0200

    Add new plugin

--- lib/essentials.dart
+++ lib/essentials.dart
@@ -8,4 +8,5 @@ import 'package:permission_handler/permission_handler.dart';
 class Essentials {
   static Geolocator geolocator = Geolocator();
   static PermissionHandler permissionHandler = PermissionHandler();
+  static GoogleApiAvailability googleApiAvailability = GoogleApiAvailability();
 }
--- test/essentials_test.dart
+++ test/essentials_test.dart
@@ -1,4 +1,5 @@
 import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:test/test.dart';
 
 import 'package:essentials/essentials.dart';
@@ -6,5 +7,7 @@ import 'package:essentials/essentials.dart';
 void main() {
   test('test plugin instances', () {
     expect(Essentials.geolocator, new Geolocator());
+    expect(Essentials.permissionHandler, new PermissionHandler());
+    expect(Essentials.googleApiAvailability, new GoogleApiAvailability());
   });
 }
