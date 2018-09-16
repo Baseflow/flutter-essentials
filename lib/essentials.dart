97e2e554d5d6fd54e39c579ee7e4f8cdf2191fa5
Martijn van Dijk <mhvdijk@gmail.com>
2018-09-16 22:12:19 +0200

    Add more plugins

--- lib/essentials.dart
+++ lib/essentials.dart
@@ -3,10 +3,13 @@ library essentials;
 import 'dart:async';
 
 import 'package:geolocator/geolocator.dart';
+import 'package:google_api_availability/google_api_availability.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:contacts_plugin/contacts_plugin.dart';
 
 class Essentials {
   static Geolocator geolocator = Geolocator();
   static PermissionHandler permissionHandler = PermissionHandler();
   static GoogleApiAvailability googleApiAvailability = GoogleApiAvailability();
+  static ContactsPlugin contactsPlugin = ContactsPlugin();
 }
--- test/essentials_test.dart
+++ test/essentials_test.dart
@@ -1,13 +1,18 @@
-import 'package:geolocator/geolocator.dart';
-import 'package:permission_handler/permission_handler.dart';
+
 import 'package:test/test.dart';
 
 import 'package:essentials/essentials.dart';
+
+import 'package:contacts_plugin/contacts_plugin.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_api_availability/google_api_availability.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   test('test plugin instances', () {
     expect(Essentials.geolocator, new Geolocator());
     expect(Essentials.permissionHandler, new PermissionHandler());
     expect(Essentials.googleApiAvailability, new GoogleApiAvailability());
+    expect(Essentials.contactsPlugin, new ContactsPlugin());
   });
 }
